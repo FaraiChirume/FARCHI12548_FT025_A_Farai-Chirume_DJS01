760d106ac4428bf845cd1f19e1b38644ae25b447
FaraiChirume <arthurc00000@gmail.com>
2025-10-26 01:02:09 +0200

    updated link

--- src/views/createGrid.js
+++ src/views/createGrid.js
@@ -1,4 +1,4 @@
-import { createPodcastCard } from "../components/createPodcastCard.js";
+import { createPodcastCard } from "../components/createPodcastCard.js ";
 import { createModal } from "../components/createModal.js";
 
 /**
